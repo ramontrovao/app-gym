078827ac3386436d6866982f3d5a2712a8c5400a
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-10 23:18:32 -0300

    feat: created Loading component

--- App.tsx
+++ App.tsx
@@ -1,23 +1,29 @@
-import { StatusBar, Text, View } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
+import { Loading } from "@components/Loading";
+
+import { THEME } from "./src/theme";
+
+import { StatusBar, Text, View } from "react-native";
+import { NativeBaseProvider } from "native-base";
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
 
-      {fontsLoaded && <Text>Home</Text>}
-      {!fontsLoaded && <View> </View>}
-    </View>
+      {fontsLoaded && <View></View>}
+      {!fontsLoaded && <Loading />}
+    </NativeBaseProvider>
   );
 }
