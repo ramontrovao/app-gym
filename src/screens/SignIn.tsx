2bafc88a32f54da8302717f800ff1008a0cd6294
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-11 22:12:26 -0300

    feat: added svg transformer

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,24 @@
-import { VStack } from "native-base";
+import { Center, Image, Text, VStack } from "native-base";
+import backgroundImage from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg";
 
 export const SignIn = () => {
-  return <VStack flex={1}></VStack>;
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Image
+        source={backgroundImage}
+        alt="Pessoas treinando"
+        resizeMode="contain"
+        position="absolute"
+      />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
+    </VStack>
+  );
 };
