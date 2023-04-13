b83626aa68149c5e3ac622fe91fc57a477603e6b
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-12 22:49:13 -0300

    feat: finished tab navigator implementation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { AuthRoutes } from "./auth.route";
 
 import { Box, useTheme } from "native-base";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { AppRoutes } from "./app.routes";
 
 export const Routes = () => {
   const nativeBaseTheme = useTheme();
@@ -11,7 +12,7 @@ export const Routes = () => {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   );
