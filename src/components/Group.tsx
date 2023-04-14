dbd7454c0f153ea09efe84d3ebc61dab90e735ed
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-13 23:46:21 -0300

    feat: added Group flatlist to Home screen

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -2,13 +2,29 @@ import { IPressableProps, Pressable, Text } from "native-base";
 
 interface IGroupProps extends IPressableProps {
   name: string;
+  isActive: boolean;
 }
 
-export const Group = ({ name, ...rest }: IGroupProps) => {
+export const Group = ({ name, isActive, ...rest }: IGroupProps) => {
   return (
-    <Pressable {...rest}>
+    <Pressable
+      mr={3}
+      w={24}
+      h={10}
+      bg="gray.600"
+      rounded="md"
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+      isPressed={isActive}
+      _pressed={{
+        borderColor: "green.500",
+        borderWidth: 1,
+      }}
+      {...rest}
+    >
       <Text
-        color="gray.200"
+        color={isActive ? "green.500" : "gray.200"}
         textTransform="uppercase"
         fontSize="xs"
         fontWeight="bold"
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,49 @@
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack } from "native-base";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export const Home = () => {
+  const [groups, setGroups] = useState([
+    "Costas",
+    "Bíceps",
+    "Tríceps",
+    "Ombro",
+  ]);
+  const [groupSelected, setGroupSelected] = useState("costas");
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <Group name="costas" />
+      <FlatList
+        data={groups}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        keyExtractor={(group) => group}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        px={8}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between">
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 };
