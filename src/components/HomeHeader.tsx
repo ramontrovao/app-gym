4f5e16b06d7efbfcfe717bea1dad7df227fcbdf7
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-13 22:47:12 -0300

    feat: added HomeHeader component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,11 @@
+import { Heading, HStack, Text } from "native-base";
+
+export const HomeHeader = () => {
+  return (
+    <HStack>
+      <Text>Olá, </Text>
+
+      <Heading>Ramon</Heading>
+    </HStack>
+  );
+};
