4c67870110e6673cc0965f1eb6d12f7a3d60d989
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-04-17 10:12:32 -0300

    feat: created ScreenHeader component

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -2,8 +2,11 @@ import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
 
 import { VStack } from "native-base";
+import { useState } from "react";
 
 export const History = () => {
+  const [exercises, setExercises] = useState([{}]);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Histórico de exercícios" />
